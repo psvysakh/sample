5d4f76ef5227a9082e059adad80648563d50b38d
psvysakh2@gmail.com <psvysakh2@gmail.com>
2019-05-31 19:47:17 +0530

    Updates

---
+++ precache-manifest.bdbf53c31327536a158cc18beb67b081.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c307612ed64fd4d4b2715a8735977c15",
+    "url": "/sample/index.html"
+  },
+  {
+    "revision": "abf4b6aea4b29bd5836e",
+    "url": "/sample/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/sample/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "abf4b6aea4b29bd5836e",
+    "url": "/sample/static/js/main.716f601c.chunk.js"
+  },
+  {
+    "revision": "4c56d8bb3462e6b62d6b",
+    "url": "/sample/static/js/runtime~main.60cb1095.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sample/static/media/logo.5d5d9eef.svg"
+  }
+]);
